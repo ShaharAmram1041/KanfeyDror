ea0ae8804acb9bc3db9958e941aa6dafa0c8279a
yirmi89 <115631964+yirmi89@users.noreply.github.com>
2023-05-05 22:45:47 +0300

    log-in

--- src/Pages/LoginPage/Login.js
+++ src/Pages/LoginPage/Login.js
@@ -1,24 +1,81 @@
-import { signInWithPopup } from "firebase/auth";
-import React from "react";
-import { auth, provider } from "../../firebase_setup/firebase";
-import { useNavigate } from "react-router-dom";
 import "./Login.css";
-export default function Login({ setIsAuth }) {
-  let nevigate = useNavigate();
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import app from "../../firebase_setup/firebase.js";
+import { useState } from 'react';
 
-  const signInWithGoogle = () => {
-    signInWithPopup(auth, provider).then((result) => {
-      localStorage.setItem("isAuth", true);
-      setIsAuth(true);
-      nevigate("/");
-    });
-  };
+
+function Login() {
+  const auth = getAuth(app);
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  // new account
+  const signUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => { 
+      const user = userCredential.user;
+      console.log(user);
+      alert("created account successfully");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      alert(errorCode)
+    })
+  }
+  // existing account  
+  const signIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user)
+    alert("signed in successfully")
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    alert(errorCode)
+  });
+
+  }
+
   return (
     <div className="LoginPage">
-      <p>Sign In With Google to Continue</p>
-      <button className="login-with-google-btn" onClick={signInWithGoogle}>
-        Sign In With Google
-      </button>
+       <input type={"email"} placeholder = "Enter Email" onChange={(e) => setEmail(e.target.value)} />
+       <input type={"password"} placeholder = "Enter Password" onChange={(e) => setPassword(e.target.value)}/>
+
+       <button onClick={signUp}> Create Account</button>
+       <button onClick={signIn}> Sign in</button>
     </div>
   );
 }
+ 
+export default Login
+
+
+
+// import { signInWithPopup } from "firebase/auth";
+//  import React from "react";
+// import { auth, provider } from "../../firebase_setup/firebase";
+// import { useNavigate } from "react-router-dom";
+// import "./Login.css";
+
+// export default function Login({ setIsAuth }) {
+//   let nevigate = useNavigate();
+
+//   const signInWithGoogle = () => {
+//     signInWithPopup(auth, provider).then((result) => {
+//       localStorage.setItem("isAuth", true);
+//       setIsAuth(true);
+//       nevigate("/");
+//     });
+//   };
+//   return (
+//     <div className="LoginPage">
+//       <p>Sign In With Google to Continue</p>
+//       <button className="login-with-google-btn" onClick={signInWithGoogle}>
+//         Sign In With Google
+//       </button>
+//     </div>
+//   );
+// }
+
+
