8467331eb5ff673c6d9e4effe8dc84e0b6af6cf4
yirmi89 <115631964+yirmi89@users.noreply.github.com>
2023-05-14 19:50:04 +0300

    table

--- src/components/Reports_Table_Component/ReportsTableCom.js
+++ src/components/Reports_Table_Component/ReportsTableCom.js
@@ -3,6 +3,13 @@ import { collection, getDocs } from "firebase/firestore";
 import { auth, db } from "../../firebase_setup/firebase";
 import { deleteDoc, doc } from "firebase/firestore";
 import { EmailForm } from "../Email_Cpomponent/EmailForm";
+import TableCell from '@mui/material/TableCell';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 function ReportsTableCom() {
   const [reports, setReports] = useState([]);
@@ -41,64 +48,73 @@ function ReportsTableCom() {
   }
 
   return (
-    <div>
-      <table style={{ borderCollapse: "collapse", width: "100%" }}>
-        <thead>
-          <tr style={{ borderBottom: "1px solid #ddd" }}>
-            <th style={{ padding: "12px 15px", textAlign: "left" }}>ID</th>
-            <th style={{ padding: "12px 15px", textAlign: "left" }}>שם</th>
-            <th style={{ padding: "12px 15px", textAlign: "left" }}>אימייל</th>
-            <th style={{ padding: "12px 15px", textAlign: "left" }}>עיר</th>
-            <th style={{ padding: "12px 15px", textAlign: "left" }}>מקום</th>
-            <th style={{ padding: "12px 15px", textAlign: "left" }}>תאריך</th>
-            <th style={{ padding: "12px 15px", textAlign: "left" }}>
-              תוכן ההודעה
-            </th>
-            <th style={{ padding: "12px 15px", textAlign: "left" }}>
-              רמת דחיפות
-            </th>
-            <th style={{ padding: "12px 15px", textAlign: "left" }}>סטטוס</th>
-            <th style={{ padding: "12px 15px", textAlign: "left" }}>
-              ביצוע פעולה
-            </th>
-            <th style={{ padding: "12px 15px", textAlign: "left" }}>הפצה</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reports.map((report) => (
-            <tr
-              key={report.id}
-              style={{
-                borderBottom: "1px solid #ddd",
-                backgroundColor:
-                  report.urgency === "דחוף" ? "Red" : "lightGreen",
-              }}
-            >
-              <td style={{ padding: "12px 15px" }}>{report.id}</td>
-              <td style={{ padding: "12px 15px" }}>{report.name}</td>
-              <td style={{ padding: "12px 15px" }}>{report.email}</td>
-              <td style={{ padding: "12px 15px" }}>{report.city}</td>
-              <td style={{ padding: "12px 15px" }}>{report.place}</td>
-              <td style={{ padding: "12px 15px" }}>{report.date}</td>
-              <td style={{ padding: "12px 15px" }}>{report.message}</td>
-              <td style={{ padding: "12px 15px" }}>{report.urgency}</td>
-              <td style={{ padding: "12px 15px" }}>{report.status}</td>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: "80vh", }}>
+      <TableContainer style={{
+        width: '80%', height: '80%', overflow: 'auto', border:
+          '1px solid black', borderRadius: '15px', background: 'white'
+      }}>
+        <Table>
+          <TableHead> 
+            <TableRow style={{ backgroundColor: 'black' }}>
+              <TableCell align="right" style={{ color: 'white' }}>שם</TableCell>
+              <TableCell align="right" style={{ color: 'white' }}>ID</TableCell>
+              <TableCell align="right" style={{ color: 'white' }}>אימייל</TableCell>
+              <TableCell align="right" style={{ color: 'white' }}>עיר</TableCell>
+              <TableCell align="right" style={{ color: 'white' }}>מקום</TableCell>
+              <TableCell align="right" style={{ color: 'white' }}>תאריך</TableCell>
+              <TableCell align="right" style={{ color: 'white' }}>תוכן הודעה</TableCell>
+              <TableCell align="right" style={{ color: 'white' }}>רמת דחיפות</TableCell>
+              <TableCell align="right" style={{ color: 'white' }}>ביצוע פעולה</TableCell>
+              <TableCell align="right" style={{ color: 'white' }}>עריכה</TableCell>
+              <TableCell align="right" style={{ color: 'white' }}>עריכה</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {reports.map((report) => (
+              <TableRow
+                key={report.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              >
+                <TableCell component="th" scope="row">
+                  {report.name}
+                </TableCell>
+                <TableCell align="right">{report.id}</TableCell>
+                <TableCell align="right">{report.email}</TableCell>
+                <TableCell align="right">{report.city}</TableCell>
+                <TableCell align="right">{report.place}</TableCell>
+                <TableCell align="right">{report.date}</TableCell>
+                <TableCell align="right">{report.message}</TableCell>
+                <TableCell align="right">
+                  <div style={{
+                    backgroundColor: report.urgency === "דחוף" ? "red" : "lightGreen",
+                    width: '50px',
+                    borderRadius: '15px',
+                    padding: '20px',
+                    margin: '0',
+                  }}>
+                    <span style={{ color: '#000000', fontWeight: 'bold', }}>{report.urgency}</span>
+                  </div>
+                </TableCell>
 
-              <td style={{ padding: "12px 15px" }}>
-                <button style={{ margin: "5px" }}>עריכה</button>
-                <button onClick={() => handleDelete(report.id)}>מחיקה</button>
-              </td>
-              <td>
-                {/* <input type="email" placeholder="אימייל"></input> */}
-                <button onClick={toggleEmailForm}>שלח מייל</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      {showEmailForm && <EmailForm />}
+                <TableCell align="right">{report.status}</TableCell>
+                <TableCell align="right">
+                  <button align={{ margin: "8px" }}>עריכה</button>
+                  <button onClick={() => handleDelete(report.id)}>מחיקה</button>
+                </TableCell>
+                <TableCell>
+                  {/* <input type="email" placeholder="אימייל"></input> */}
+                  <button onClick={toggleEmailForm}>שלח מייל</button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
 
 export default ReportsTableCom;
+
+
+
