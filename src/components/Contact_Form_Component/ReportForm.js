7ca51b892e45140b6c9e38c7341b5a2257d3bffa
liors16 <liorsol1610@gmail.com>
2023-05-17 19:05:09 +0300

    ReportForm.js-Cities-Drop-Down-List

--- src/components/Contact_Form_Component/ReportForm.js
+++ src/components/Contact_Form_Component/ReportForm.js
@@ -1,15 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { uid } from "uid";
 import styles from "./ReportFormStyle.module..css";
 import { db } from "../../firebase_setup/firebase";
 import { addDoc, collection } from "firebase/firestore";
+
 export default function ReportForm() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
-  const [city, setCity] = useState("");
+  const [city] = useState("");
   const [place, setPlace] = useState("");
   const [date, setDate] = useState("");
+  
+  const api_url = "https://data.gov.il/api/3/action/datastore_search";
+  const cities_resource_id = "5c78e9fa-c2e2-4771-93ff-7f400a12f7ba";
+  const city_name_key = "שם_ישוב";
+  const [cities, setCities] = useState([]);
+  const [selectedCity, setSelectedCity] = useState("");
 
   const writeReportToDB = async (e) => {
     e.preventDefault();
@@ -26,11 +34,43 @@ export default function ReportForm() {
         status: "בטיפול",
         uuid,
       });
-      // alert("Your ID is: ", docRef.id);
       alert("Your ID is: " + docRef.id);
     } catch (e) {
       console.error("Error adding document: ", e);
     }
+  };
+
+
+  useEffect(() => {
+    populateDataList(cities_resource_id, city_name_key, setCities);
+  }, []);
+
+  const populateDataList = (resourceId, fieldName, setState) => {
+    axios
+      .get(api_url, {
+        params: {
+          resource_id: resourceId,
+          limit: 32000,
+        },
+        responseType: "json",
+      })
+      .then((response) => {
+        const records = response?.data?.result?.records || [];
+        const dataList = records.map((record) => record[fieldName].trim());
+        setState(dataList);
+      })
+      .catch((error) => {
+        console.log("Couldn't get list for", fieldName, error);
+      });
+  };
+
+  const handleCityChange = (event) => {
+    const selectedCity = event.target.value;
+    setSelectedCity(selectedCity);
+    populateDataList(
+      { שם_ישוב: selectedCity },
+      32000
+    );
   };
 
   return (
@@ -48,7 +88,7 @@ export default function ReportForm() {
 
       <div>
         <label>אימייל</label>
-        <input
+        <input 
           placeholder="אימייל"
           value={email}
           dir="rtl"
@@ -56,15 +96,26 @@ export default function ReportForm() {
         />
       </div>
 
+      
       <div>
         <label>עיר בה קרה המקרה *</label>
-        <input
-          placeholder="נא לבחור עיר"
-          value={city}
-          dir="rtl"
-          onChange={(e) => setCity(e.target.value)}
-        />
+        <div className="city-dropdown-container">
+          <input
+            list="cities"
+            value={selectedCity}
+            onChange={handleCityChange}
+            dir="rtl"
+          />
+          <datalist id="cities">
+            {cities
+              .filter((city) => city !== "לא רשום") // Filter out the "לא רשום" option
+              .map((city) => (
+                <option key={city} value={city} />
+              ))}
+          </datalist>
+        </div>
       </div>
+  
 
       <div>
         <label>איפה *</label>
