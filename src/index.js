2fefdd6127f46df8df3f07e19154d139a8babf55
Shahar Amram <126160304+ShaharAmram1041@users.noreply.github.com>
2023-06-04 13:55:12 +0300

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client"; // Import createRoot from "react-dom/client"
-import App from "./App";
-import "./Styles/index.scss";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
